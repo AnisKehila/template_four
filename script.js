10caa9db10c9669063f11451cc5eb4b5c1a507dd
--global <--global>
2022-10-28 13:56:22 -0700

    finished settings page + working on profile

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 function toggleClass() {
-    if(this.classList.contains('active')) {
-        this.classList.remove('active');
-    } else {
-        this.classList.add('active');
-    }
+    this.classList.toggle('active');
+}
+function closeSideBar(sideBar) {
+    sideBar.style = `
+        margin-left: -13%; 
+    `;
+}
+function showSideBar(sideBar) {
+    sideBar.style = `margin-left: 0;`;
+
 }
 // toggle button
 let toggleBtns = document.querySelectorAll('.switch-btn');
 toggleBtns.forEach(e => {
     e.addEventListener('click' , toggleClass);
 })
+
+// close side bar in mobile if scrolled 
+let sideBar = document.getElementById('side-bar');
+let closeOnY = document.getElementById('close-side-bar').offsetTop + 100;
+let body = document.body;
+window.addEventListener('scroll' , (e) => {
+    if(this.scrollY > closeOnY && this.innerWidth < 768) {
+        closeSideBar(sideBar);
+    } else {
+        showSideBar(sideBar);
+    }
+} 
+);
